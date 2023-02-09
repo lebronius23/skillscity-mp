fd7d6f457f8f587f4cb6b5398354adf127e2e6b5
lebronius23 <lebronius23@gmail.com>
2023-02-09 12:47:56 +0000

    added content component to books page and homepage

--- pages/books.js
+++ pages/books.js
@@ -1,15 +1,21 @@
 import BooksGrid from "@/components/books/books-grid"
 import Header from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
+import Content from "@/components/shared/content";
+import Link from "next/link";
 
 export default function BooksPage() {
     return (
         <div className="w-full">
             <Header name="Books"/>
+
+            <Content>
+                <div className="w-full max-w-6xl mx-auto mt-8">
+                    <BooksGrid/>
+                </div>
+            </Content>   
             
-            <div className="w-full max-w-6xl mx-auto mt-8">
-                <BooksGrid/>
-            </div>
+           
 
             <Footer title="Home Page" href="/" />    
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+import Hero from "@/components/home/hero";
+import Topics from "@/components/home/topics";
+import Content from "@/components/shared/content";
 import Footer from "@/components/shared/footer";
 import Header from "@/components/shared/header";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
@@ -19,12 +23,29 @@ const handleClick=() => {
 return(
   <div className="w-full">
     <Header name="Home"/>
-    <button
-        className="bg-blue-500 px-2 py-1 rounded"
-        onClick={handleClick}
-        >
-          Click me!!!
-        </button>
+
+    <Hero 
+      imgUrl="/assets/test.jpeg"
+      title= "Vytautas"
+      subtitle="I am a bootcamp student"
+    />
+
+    <Content>
+      <div className="w-full flex flex-col">
+
+        <Topics />
+
+        <div>
+          <button
+            className="bg-blue-500 px-2 py-1 rounded"
+            onClick={handleClick}
+            >
+              Click me!!!
+          </button>
+        </div>
+        </div>
+    </Content>
+    
 
       <Footer href="/books" title="Books Page" />
   </div>
