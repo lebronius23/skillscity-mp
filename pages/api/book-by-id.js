7912f4c97b9595d0f49d4ccd7fc933bb784047de
lebronius23 <lebronius23@gmail.com>
2023-02-09 17:41:03 +0000

    added link to purchase for the books

--- pages/api/book-by-id.js
+++ pages/api/book-by-id.js
@@ -6,19 +6,23 @@ export default function handler(req,res) {
             id:"1",
             title: "The Hobbit",
             author: "J.R.R. Tolkien",
-            imgUrl: "/assets/1.jpg"
+            imgUrl: "/assets/1.jpg",
+            linkToPurchase:"https://www.amazon.co.uk/Hobbit-J-R-Tolkien-ebook/dp/B002RI9ZY0/ref=sr_1_2?keywords=hobbit&qid=1675964182&s=books&sprefix=hobbit%2Cstripbooks%2C93&sr=1-2"
         },
         {
             id:"2",
             title: "The Fellowship of the Ring",
             author: "J.R.R Tolkien",
-            imgUrl: "/assets/2.jpg"
+            imgUrl: "/assets/2.jpg",
+            linkToPurchase:"https://www.amazon.co.uk/Lord-Rings-J-R-Tolkien/dp/0261103253"
         },
         {
             id:"3",
             title: "Born a Crime",
             author: "Trevor Noah",
-            imgUrl: "/assets/3.jpg"
+            imgUrl: "/assets/3.jpg",
+            linkToPurchase: "https://www.amazon.co.uk/Born-Crime-Stories-African-Childhood/dp/1473635306"
+            
         }
     ];
 
