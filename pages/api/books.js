709f6711d432c835362f86f69b71cfc93e330101
lebronius23 <lebronius23@gmail.com>
2023-02-09 15:48:52 +0000

    added images and a like button to each book

--- pages/api/books.js
+++ pages/api/books.js
@@ -3,14 +3,17 @@ export default function handler(req,res) {
         {
             title: "The Hobbit",
             author: "J.R.R. Tolkien",
+            imgUrl: "/assets/1.jpg"
         },
         {
             title: "The Fellowship of the Ring",
-            author: "J.R.R Tolkien"
+            author: "J.R.R Tolkien",
+            imgUrl: "/assets/2.jpg"
         },
         {
             title: "Born a Crime",
-            author: "Trevor Noah"
+            author: "Trevor Noah",
+            imgUrl: "/assets/3.jpg"
         }
     ];
 
