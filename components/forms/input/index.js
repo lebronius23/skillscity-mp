6e0de9170394e0f4e05f1deaff9a5153c3e70ea6
lebronius23 <lebronius23@gmail.com>
2023-02-22 17:28:19 +0000

    added mobile responsive styles to contact page

--- components/forms/input/index.js
+++ components/forms/input/index.js
@@ -7,7 +7,7 @@ export default function Input(props) {
     return (
         <input
             {...props}
-            className="bg-gray-200 py-3 px-6 border w-96 rounded"
+            className="bg-gray-200 py-3 px-6 border w-full rounded"
             onChange={handleChange}
         />
     )
