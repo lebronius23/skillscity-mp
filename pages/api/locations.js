f7466a937751407e9ef280e17e43bf4616e0c95b
lebronius23 <lebronius23@gmail.com>
2023-03-02 15:40:24 +0000

    introduced locations api call to get location from database

--- pages/api/locations.js
+++ pages/api/locations.js
@@ -1,24 +1,9 @@
-export default function handler(req,res) {
-    const locations = [
-        {
-            id:"new-york",
-            title: "New York",
-            visitedDate: "Today",
-            imgUrl: "/assets/new-york.png"
-        },
-        {
-            id:"singapore",
-            title: "Singapore",
-            visitedDate: "Last year",
-            imgUrl: "/assets/singapore.png"
-        },
-        {
-            id:"san-francisco",
-            title: "San Francisco",
-            author: "Jan 2022",
-            imgUrl: "/assets/san-francisco.png"
-        }
-    ];
+import sql from "@/utils/postgres";
+
+export default async function handler(req,res) {
+    const locations = await sql`
+        select * from locations
+        `;
 
     res.json(locations);
 }
