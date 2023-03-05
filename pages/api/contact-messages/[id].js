3d30007d8a4a311b2bb8973a1bbf52f451318a9f
lebronius23 <lebronius23@gmail.com>
2023-03-05 13:30:46 +0000

    added console.log to id

--- pages/api/contact-messages/[id].js
+++ pages/api/contact-messages/[id].js
@@ -2,6 +2,7 @@ import pg from "@/utils/pg";
 
 export default async function handler(req, res) {
     const { id } = req.query;
+    console.log(id);
 
     await pg.execute(`
         delete from contact_messages where id = ${id}
